765cf23d9e90f31fe7f67dce9406b86d6687b58e
farreldevara <farel199@yahoo.com>
2024-04-02 19:06:42 +0700

    feat:layout home

--- hackben/src/app/(beforeLogin)/login/page.tsx
+++ hackben/src/app/(beforeLogin)/login/page.tsx
@@ -4,7 +4,7 @@ export default function Login() {
   return (
     <>
       {/* component */}
-      <div className="bg-gray-100 flex justify-center items-center h-screen">
+      <div className="bg-white flex justify-center items-center h-screen">
         {/* Right: Login Form */}
         <div className="lg:p-36 md:p-52 sm:20 p-8 w-full lg:w-1/2">
           <h1 className="text-2xl font-semibold mb-4 text-red-500">Login</h1>
