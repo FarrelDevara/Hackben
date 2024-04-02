832eb06ad45bc2ea578ab3b94e4c63d5b796530b
farreldevara <farel199@yahoo.com>
2024-04-02 17:15:47 +0700

    feat:layout register&login

---
+++ hackben/src/app/(beforeLogin)/login/page.tsx
@@ -0,0 +1,78 @@
+import Image from 'next/image';
+
+export default function Login() {
+  return (
+    <>
+      {/* component */}
+      <div className="bg-gray-100 flex justify-center items-center h-screen">
+        {/* Right: Login Form */}
+        <div className="lg:p-36 md:p-52 sm:20 p-8 w-full lg:w-1/2">
+          <h1 className="text-2xl font-semibold mb-4 text-red-500">Login</h1>
+          <form
+            action="#"
+            method="POST"
+          >
+            {/* Username Input */}
+            <div className="mb-4">
+              <label
+                htmlFor="username"
+                className="block text-black"
+              >
+                Username
+              </label>
+              <input
+                type="text"
+                id="username"
+                name="username"
+                className="w-full border border-gray-300 rounded-md py-2 px-3 focus:outline-none focus:border-red-500"
+                autoComplete="off"
+              />
+            </div>
+            {/* Password Input */}
+            <div className="mb-4">
+              <label
+                htmlFor="password"
+                className="block text-gray-600"
+              >
+                Password
+              </label>
+              <input
+                type="password"
+                id="password"
+                name="password"
+                className="w-full border border-gray-300 rounded-md py-2 px-3 focus:outline-none focus:border-red-500"
+                autoComplete="off"
+              />
+            </div>
+            {/* Login Button */}
+            <button
+              type="submit"
+              className="bg-red-500 hover:bg-red-600 text-white font-semibold rounded-md py-2 px-4 w-full"
+            >
+              Login
+            </button>
+          </form>
+          {/* Sign up  Link */}
+          <div className="mt-6 text-black text-center font-semibold">
+            <h1 className='mb-3'>Don't have an account?</h1>
+            <h1 className='mb-3'>Become a HackBen Friend and get a special offer!</h1>
+          </div>
+          <button
+              type="submit"
+              className="bg-red-500 hover:bg-red-600 text-white font-semibold rounded-md py-2 px-4 w-full"
+            >
+              Register
+            </button>
+        </div>
+        {/* Left: Image */}
+        <div className="w-1/2 h-screen hidden lg:block mt-20">
+          <img
+            src="https://www.hokben.co.id/assets/img/img1.jpeg"
+            alt="Placeholder Image"
+            className="mt-20 items-center"
+          />
+        </div>
+      </div>
+    </>
+  );
+}
---
+++ hackben/src/app/(beforeLogin)/register/page.tsx
@@ -0,0 +1,85 @@
+import Image from 'next/image';
+
+export default function Register() {
+  return (
+    <main>
+      {/* component */}
+      <div className="bg-gray-100 flex justify-center items-center h-screen">
+        {/* Right: Login Form */}
+        <div className="lg:p-36 md:p-52 sm:20 p-8 w-full lg:w-1/2 ">
+          <h1 className="text-2xl font-bold mb-4 text-red-600">Register</h1>
+          <h2 className="font-bold mb-4">Personal Data</h2>
+          <form
+            action="#"
+            method="POST"
+          >
+            {/* Username Input */}
+            <div className="mb-4">
+              <label
+                htmlFor="name"
+                className="block text-gray-600"
+              >
+                Name
+              </label>
+              <input
+                type="text"
+                id="name"
+                name="name"
+                className="w-full border border-gray-300 rounded-md py-2 px-3 focus:outline-none focus:border-red-500"
+                autoComplete="off"
+              />
+            </div>
+             {/* Email Input */}
+             <div className="mb-4">
+              <label
+                htmlFor="email"
+                className="block text-gray-600"
+              >
+                Email
+              </label>
+              <input
+                type="email"
+                id="email"
+                name="email"
+                className="w-full border border-gray-300 rounded-md py-2 px-3 focus:outline-none focus:border-red-500"
+                autoComplete="off"
+              />
+            </div>
+            {/* Password Input */}
+            <div className="mb-4">
+              <label
+                htmlFor="password"
+                className="block text-gray-600"
+              >
+                Password
+              </label>
+              <input
+                type="password"
+                id="password"
+                name="password"
+                className="w-full border border-gray-300 rounded-md py-2 px-3 focus:outline-none focus:border-red-500"
+                autoComplete="off"
+              />
+            </div>
+            {/* Login Button */}
+            <button
+              type="submit"
+              className="bg-red-500 hover:bg-red-600 text-white font-semibold rounded-md py-2 px-4 w-full"
+            >
+              Register
+            </button>
+          </form>
+          {/* Sign up  Link */}
+          <div className="mt-6 text-white-500 text-center">
+            <a
+              href="#"
+              className="hover:underline"
+            >
+              Already have an account? Login
+            </a>
+          </div>
+        </div>
+      </div>
+    </main>
+  );
+}
